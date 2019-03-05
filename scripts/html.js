8094af3215c6530a7f5375b83186825db1c26b29
Eric Mauldin <emauldin84@gmail.com>
2019-03-05 12:11:16 -0500

    adds printEntries function

--- scripts/html.js
+++ scripts/html.js
@@ -1,12 +1,32 @@
-const addressBook = {
-    DigitalCrafts: {
-        phone: '404-123-1234', 
-        address: '3423 Piedmont', 
-        city: 'Atlanta',
-    },
-    "Chris Aquino": {
-        phone: '404-999-1234', 
-        address: '3421 Piedmont', 
-        city: 'Atlanta',
-    },
-};
+
+    const addressBook = {
+        DigitalCrafts: {
+            phone: '404-123-1234', 
+            address: '3423 Piedmont', 
+            city: 'Atlanta',
+        },
+        "Chris Aquino": {
+            phone: '404-999-1234', 
+            address: '3421 Piedmont', 
+            city: 'Atlanta',
+        },
+    };
+// function printEntries(book) {
+//     const allTheKeys = Object.keys(book);
+
+//     allTheKeys.forEach(function (stringKey) {
+//         let entry = book[stringKey];
+//         console.log(entry);
+//     });
+    
+// }
+
+function printEntries(book, item) {
+    const allTheKeys = Object.keys(book);
+
+    allTheKeys.forEach(function (stringKey) {
+        let entry = book[stringKey];
+        console.log(entry[item]);
+    });
+    
+}
